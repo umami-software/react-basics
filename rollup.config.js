4a4182f8e9f8a20f73def0cec1dcad513d2f6661
Mike Cao <mike@mikecao.com>
2022-09-19 23:15:51 -0700

    Generate custom class names.

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import ts from 'rollup-plugin-ts';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
@@ -5,13 +6,14 @@ import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import svgr from '@svgr/rollup';
+import md5 from 'md5';
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: 'dist/cjs/index.js',
+        file: 'dist/react-basics.js',
         format: 'cjs',
         name: 'react-basics',
       },
@@ -22,7 +24,20 @@ export default [
     ],
     plugins: [
       external(),
-      postcss({ extract: true, modules: true, minimize: true }),
+      postcss({
+        extract: true,
+        minimize: true,
+        modules: {
+          generateScopedName: function (name, filename, css) {
+            const file = path.basename(filename, '.css').replace('.module', '');
+            const hash = Buffer.from(md5(`${name}:${filename}:${css}`))
+              .toString('base64')
+              .substring(0, 5);
+
+            return `${file}-${name}--${hash}`;
+          },
+        },
+      }),
       svgr({ icon: true }),
       resolve(),
       commonjs(),
