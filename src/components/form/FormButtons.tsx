547da5d20a106cc031a6b7e6ecdd8bdb05a5f2b2
Mike Cao <mike@mikecao.com>
2022-09-26 01:54:18 -0700

    Added alignment prop to FormButtons.

--- src/components/form/FormButtons.tsx
+++ src/components/form/FormButtons.tsx
@@ -1,5 +1,6 @@
 import { CommonProps } from 'types';
 import classNames from 'classnames';
+import { addClassNames } from 'components/utils';
 import styles from './FormButtons.module.css';
 
 export interface FormButtonsProps extends CommonProps {
@@ -7,10 +8,17 @@ export interface FormButtonsProps extends CommonProps {
 }
 
 export function FormButtons(props: FormButtonsProps) {
-  const { className, style, children } = props;
+  const { align = 'left', className, style, children } = props;
 
   return (
-    <div className={classNames(styles.buttons, className)} style={style}>
+    <div
+      className={classNames(
+        styles.buttons,
+        className,
+        addClassNames(styles, { align: { value: align, map: ['left', 'center', 'right'] } }),
+      )}
+      style={style}
+    >
       {children}
     </div>
   );
