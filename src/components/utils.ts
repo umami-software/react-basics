895883c2f987729bedc13a82dac39baeafde51b1
Mike Cao <mike@mikecao.com>
2022-10-01 16:03:05 -0700

    Removed duplicate functions.

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -61,29 +61,6 @@ export function cloneChildren(
   });
 }
 
-export function shuffleArray(a) {
-  const arr = a.slice();
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-  return arr;
-}
-
-export function getRandomChars(
-  n,
-  chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-) {
-  const arr = shuffleArray(chars.split(''));
-  let s = '';
-  for (let i = 0; i < n; i++) {
-    s += arr[Math.floor(Math.random() * arr.length)];
-  }
-  return s;
-}
-
 export function ensureArray(arr?: any) {
   if (arr === undefined || arr === null) return [];
   if (Array.isArray(arr)) return arr;
