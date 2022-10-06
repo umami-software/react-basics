541357504337b271ff22d8a8d6c87b6e0b1708e5
Mike Cao <mike@mikecao.com>
2022-10-05 19:02:56 -0700

    Updated Loading props.

--- src/components/status/Loading.tsx
+++ src/components/status/Loading.tsx
@@ -5,12 +5,12 @@ import Dots from 'components/status/Dots';
 import Spinner from 'components/status/Spinner';
 
 export interface LoadingProps extends CommonProps {
-  size: 'small' | 'medium' | 'large';
-  variant: 'dots' | 'spinner';
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'dots' | 'spinner';
 }
 
 export function Loading(props: LoadingProps) {
-  const { size, variant = 'spinner', className, style } = props;
+  const { size = 'large', variant = 'spinner', className, style } = props;
   return (
     <div className={classNames(styles.loading, className)} style={style}>
       {variant === 'dots' && <Dots size={size} />}
