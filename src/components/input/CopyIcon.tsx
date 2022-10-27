9affa8403d910b7bedb3922ba0a3fe1a2a5cb80e
Mike Cao <mike@mikecao.com>
2022-10-27 09:08:01 -0700

    Added CopyIcon.

--- src/components/input/CopyIcon.tsx
+++ src/components/input/CopyIcon.tsx
@@ -1,10 +1,13 @@
 import { Copy } from 'icons';
+import classNames from 'classnames';
 import Icon from 'components/common/Icon';
 import CopyTrigger, { CopyTriggerProps } from 'components/input/CopyTrigger';
+import styles from './CopyIcon.module.css';
 
 export function CopyIcon(props: CopyTriggerProps) {
+  const { className, ...rest } = props;
   return (
-    <CopyTrigger {...props}>
+    <CopyTrigger className={classNames(styles.clickable, className)} {...rest}>
       <Icon>
         <Copy />
       </Icon>
