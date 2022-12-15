0c9650fc0f4c876e348e730fa96fc0bdeb70d718
Mike Cao <mike@mikecao.com>
2022-12-15 14:47:31 -0800

    Updated Tooltip render.

--- src/components/overlay/Tooltip.tsx
+++ src/components/overlay/Tooltip.tsx
@@ -2,14 +2,6 @@ import classNames from 'classnames';
 import { Popup, PopupProps } from 'components/overlay/Popup';
 // eslint-disable-next-line css-modules/no-unused-class
 import styles from './Tooltip.module.css';
-import Icon from 'components/common/Icon';
-
-const icons = {
-  top: 'triangle-down',
-  bottom: 'triangle-up',
-  left: 'triangle-right',
-  right: 'triangle-left',
-};
 
 export interface TooltipProps extends PopupProps {
   title?: string;
@@ -22,7 +14,6 @@ export function Tooltip(props: TooltipProps) {
     <Popup position={position} show={show} gap={gap}>
       <div {...domProps} className={classNames(styles.tooltip, className, styles[position])}>
         <div className={styles.body}>{children}</div>
-        <Icon icon={icons[position]} className={styles.icon} />
       </div>
     </Popup>
   );
