020bed6364d9c4afa4e1ee6b88e35cf21b0f2ccf
mikecao <moocao@gmail.com>
2023-06-04 05:42:49 +0000

    Deploying to gh-pages from @ umami-software/react-basics@bc08c18964843b3b75e25d299a4f17d59a2da183 🚀

---
+++ sb-addons/storybook-9/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var p=__STORYBOOKADDONS__,{addons:O,types:S,mockChannel:f}=__STORYBOOKADDONS__;O.setConfig({enableShortcuts:!1});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
